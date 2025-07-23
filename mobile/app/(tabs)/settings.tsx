9636c1e682c476ffa86b19ea49f2680d3f6aafde
amirrajj <amiramraja@gmail.com>
2025-07-23 22:54:27 +0330

    added some necessary project tabs and screens🐨💙

---
+++ mobile/app/(tabs)/settings.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
+import { getTokens } from '@/configs/helpers';
+
+const Settings = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  )
+}
+
+export default Settings
